96c285061597bb4ba1d3c1b7f776282b4f511068
Ale-mariano <alemariano29@gmail.com>
2022-08-31 19:59:15 -0300

    Objetos compostos

---
+++ Treinamento04/src/treinamento04/Treinamento04.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treinamento04;
+
+/**
+ *
+ * @author alema
+ */
+public class Treinamento04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
