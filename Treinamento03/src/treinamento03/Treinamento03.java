528bb16a62d20243f8c8e8527088f769b03362a5
Ale-mariano <alemariano29@gmail.com>
2022-08-31 17:09:38 -0300

    Encapsulamento

---
+++ Treinamento03/src/treinamento03/Treinamento03.java
@@ -0,0 +1,14 @@
+package treinamento03;
+public class Treinamento03 {
+
+    public static void main(String[] args) {
+       ControleRemoto c = new ControleRemoto();
+       c.ligar();
+       c.desligar();
+       c.maisVolume();
+       c.play();
+       c.abrirMenu();
+       c.fecharMenu();
+    }
+    
+}
